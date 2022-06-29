3cb7c92a656eb2a29d3f6218b127b1b0f5ac3d6d
TAS745 <teguhsalim@onmail.com>
2022-06-29 20:40:55 +0700

    Qn 1-4 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for (let i=1; i <= string.length; i++) {
+        if(i===1) {
+            reverse = reverse.concat(string.slice(-i));
+            continue;
+        }
+        reverse = reverse.concat(string.slice(-i,-i+1));
+    }
+    return reverse;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    const args = [...arguments];
+    let arrayNew = args[0];
+    for (let i = 1; i < args.length; i++) {
+         arrayNew = arrayNew.filter(function(item) {
+            return item !== args[i];
+         })
+    }
+    return arrayNew;
 };
 
 // Do not edit below this line
