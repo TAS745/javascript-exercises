215be7f2c5e9678847051d7ecda5f6db2ed62c8f
TAS745 <teguhsalim@onmail.com>
2022-06-29 21:27:56 +0700

    Qn 5-7 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const args = [num1, num2];
+    args.sort();
 
+    let sum = 0;
+
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }else if(typeof num1 === "number" && typeof num2 === "number") {
+        for (let i = args[0]; i <= args[1]; i++) {
+            sum += i;
+        }
+    }else {
+        return "ERROR";
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempF = 5*(temp-32)/9;
+  return Number(tempF.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempC = (9*temp/5)+32;
+  return Number(tempC.toFixed(1));
 };
 
 // Do not edit below this line
